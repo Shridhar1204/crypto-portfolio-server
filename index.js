0e6addb305d12d5578cb5bdaade388935a6b0876
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-08-07 22:11:07 +0530

    port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
